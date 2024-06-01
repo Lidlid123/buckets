d782fc6dfcb939394d51b97d6bf7cb38394f61e1
lidor koresh <lidor123@gmail.com>
2024-06-01 17:19:56 +0200

    fixed script

--- script.sh
+++ script.sh
@@ -12,7 +12,7 @@ sudo apt install python3-setuptools -y
 # Install Flask using pip3
 pip3 install Flask
 
-git clone https://github.com/Lidlid123/terraform.git --branch website
+git clone https://github.com/Lidlid123/terraform.git --branch loadbalancer
 
 nohup sudo -E python3 app.py 2>&1 >/dev/null &
 
